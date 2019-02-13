183c7cae5347d1326e53f3001608e7cb71e4fd98
Gerardo Baez <g@gerardobaez.com>
2019-02-13 18:22:02 -0400

    Add contact form to site

--- src/assets/scripts/site.js
+++ src/assets/scripts/site.js
@@ -1,63 +1,102 @@
 (function (hc) {
 
+    /**
+     * Show like & dislike buttons and hide everything else.
+     */
     function showLikeDisklikeButtons () {
         $('.rate-article form').hide();
         $('.rate-article .main').show();
     }
 
+    /**
+     * Show article feedback form and hide like/dislike buttons.
+     */
     function showArticleFeedbackForm () {
+        document.getElementById('feedback').value = ''
         $('.rate-article form').show();
         $('.rate-article .main').hide();
     }
 
-    function rateArticleSuccessMessage (message) {
-        $('.rate-article .text-success').text(message);
+    /**
+     * Show 'like sent' success message.
+     */
+    function likeSent () {
+        $('.rate-article .rating-sent').show();
         $('.rate-article form').hide();
         $('.rate-article .main').hide();
     }
 
+    /**
+     * Show 'feedback sent' success message.
+     */
+    function feedbackSent () {
+        $('.rate-article .feedback-sent').show();
+        $('.rate-article form').hide();
+        $('.rate-article .main').hide();
+    }
+
+    /**
+     * Send article's like.
+     *
+     * @param {Number} articleId
+     */
     function sendLike (articleId) {
-        startLoadingState()
+        startLoadingState();
 
-        $.post(`/article/${articleId}/rating`, {
+        $.post(`/${KnowledgeBaseApp.site.locale}/article/${articleId}/rating`, {
+            _token: KnowledgeBaseApp.site.csrfToken,
             type: 'like'
-        }, function (response) {
-            console.log(response)
-
-            resetLoadingState()
-        })
+        }, likeSent).always(resetLoadingState);
     }
 
+    /**
+     * Send article's dislike.
+     *
+     * @param {Number} articleId
+     */
     function sendDislike (articleId) {
-        startLoadingState()
+        startLoadingState();
 
-        $.post(`/article/${articleId}/rating`, {
+        $.post(`/${KnowledgeBaseApp.site.locale}/article/${articleId}/rating`, {
+            _token: KnowledgeBaseApp.site.csrfToken,
             type: 'dislike'
-        }, function (response) {
-            console.log(response)
-
-            resetLoadingState()
-        })
+        }, showArticleFeedbackForm).always(resetLoadingState);
     }
 
+    /**
+     * Send article's feedback.
+     *
+     * @param {Number} articleId
+     */
+    function sendFeedback (articleId) {
+        startLoadingState();
+
+        $.post(`/${KnowledgeBaseApp.site.locale}/article/${articleId}/feedback`, {
+            _token: KnowledgeBaseApp.site.csrfToken,
+            feedback: document.getElementById('feedback').value
+        }, feedbackSent).always(resetLoadingState);
+    }
+
+    /**
+     * Start the loading state.
+     */
     function startLoadingState () {
-        $('.rate-article').addClass('loading')
-        $('.btn').prop('disabled', true)
+        $('.rate-article').addClass('loading');
+        $('.btn').prop('disabled', true);
     }
 
+    /**
+     * End the loading state.
+     */
     function resetLoadingState () {
-        $('.rate-article').removeClass('loading')
-        $('.btn').prop('disabled', false)
+        $('.rate-article').removeClass('loading');
+        $('.btn').prop('disabled', false);
     }
 
-    hc.showLikeDisklikeButtons = showLikeDisklikeButtons
-    hc.showArticleFeedbackForm = showArticleFeedbackForm
-    hc.rateArticleSuccessMessage = rateArticleSuccessMessage
-    hc.sendLike = sendLike
-    hc.sendDislike = sendDislike
+    hc.showLikeDisklikeButtons = showLikeDisklikeButtons;
+    hc.showArticleFeedbackForm = showArticleFeedbackForm;
+    hc.sendLike = sendLike;
+    hc.sendDislike = sendDislike;
+    hc.sendFeedback = sendFeedback;
 
 })(window.helpcenter = (window.helpcenter || {}))
-
-$(document).ready(function () {
-    helpcenter.showLikeDisklikeButtons()
-})
